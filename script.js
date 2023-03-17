7dfa7af98bd23afd02cbcc9f24ef05d3891a3218
Joangisbert86 <128126616+Joangisbert86@users.noreply.github.com>
2023-03-17 13:26:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Bienvenidos a la plataforma');
+
+var titulo = document.querySelector('h1');
+titulo.addEventListener('click', function() {
+  alert('Has hecho clic en el título');
+});
